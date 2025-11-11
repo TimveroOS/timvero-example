ea9606df300860cbbfc90495433e8771e7be6eac
Yauheni Nikanovich <eugen.nikanovich@timvero.com>
2025-11-11 14:30:29 +0100

    CU-869b1d8wp Example | History | Documentation

---
+++ src/main/java/com/timvero/example/admin/participant/history/ParticipantApplicationStatusChangedHistorySupplier.java
@@ -0,0 +1,30 @@
+package com.timvero.example.admin.participant.history;
+
+import com.timvero.base.audit.impl.AbstractRelatedEntityModifiedHistorySupplier;
+import com.timvero.example.admin.application.entity.Application;
+import com.timvero.example.admin.application.entity.Application_;
+import com.timvero.example.admin.participant.entity.Participant;
+import java.util.Set;
+import java.util.UUID;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ParticipantApplicationStatusChangedHistorySupplier
+    extends AbstractRelatedEntityModifiedHistorySupplier<UUID, Participant, UUID, Application> {
+
+    public static final String APPLICATION_STATUS_CHANGED = "APPLICATION_STATUS_CHANGED";
+
+    public ParticipantApplicationStatusChangedHistorySupplier() {
+        super(Participant.class, Application.class, APPLICATION_STATUS_CHANGED);
+    }
+
+    @Override
+    protected Set<String> getTrackedFields() {
+        return Set.of(Application_.STATUS);
+    }
+
+    @Override
+    protected Application getRelatedEntity(Participant participant) {
+        return participant.getApplication();
+    }
+}
