9d3ed5acc99c5fc441142b9e2bfb09515eb41cb2
Sergey Alexetovich <sergey.alexetovich@timvero.com>
2025-11-19 16:15:06 +0100

    CU-869a3ed1d - Standardize forms in example project

--- src/main/java/com/timvero/example/ExampleApplication.java
+++ src/main/java/com/timvero/example/ExampleApplication.java
@@ -11,21 +11,21 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 
 public class ExampleApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
         SpringApplicationBuilder parentBuilder =
             new SpringApplicationBuilder(BaseConfiguration.class, CustomConfiguration.class)
-                    .web(WebApplicationType.NONE);
+                .web(WebApplicationType.NONE);
         parentBuilder.run(args);
-	    parentBuilder.child(WebMvcConfig.class, CustomWebConfiguration.class)
-	    .properties("server.port=8081")
-	    .run(args);
-	    parentBuilder.child(PortalWebConfiguration.class)
-	    .properties("server.port=8082")
-	    .run(args);
+        parentBuilder.child(WebMvcConfig.class, CustomWebConfiguration.class)
+            .properties("server.port=8081")
+            .run(args);
+        parentBuilder.child(PortalWebConfiguration.class)
+            .properties("server.port=8082")
+            .run(args);
 
         parentBuilder.child(ExternalProcessWebMvcConfig.class)
-        .properties("spring.config.name=workflow")
-        .run(args);
-	}
+            .properties("spring.config.name=workflow")
+            .run(args);
+    }
 
 }
