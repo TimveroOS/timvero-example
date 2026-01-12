bb7c2f0b64bc5c82c26e1121e6af968b84c0713e
Iliya Sinkevich <iliya.sinkevich@timvero.com>
2026-01-12 15:45:46 +0300

    CU-869b2dqb2 - Accruals implementation

--- src/main/java/com/timvero/example/admin/operation/accrual/InterestAccrualEngine.java
+++ src/main/java/com/timvero/example/admin/operation/accrual/InterestAccrualEngine.java
@@ -4,37 +4,22 @@ import static com.timvero.example.admin.credit.CreditCalculationConfiguration.IN
 import static com.timvero.example.admin.credit.CreditCalculationConfiguration.PRINCIPAL;
 
 import com.timvero.example.admin.credit.entity.ExampleCredit;
-import com.timvero.example.admin.operation.pastdue.PastDueOperation;
-import com.timvero.servicing.engine.distribution.SnapshotRecord;
 import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.Collection;
-import java.util.NavigableMap;
 import java.util.Set;
-import java.util.TreeMap;
 
-public class InterestAccrualEngine extends BasisAccrualEngine {
+public class InterestAccrualEngine extends AbstractAccrualEngine {
 
     @Override
     public String getAccrualAccount() {
         return INTEREST;
     }
 
-    @Override
     public Set<String> getBasisAccounts() {
         return Set.of(PRINCIPAL);
     }
 
     @Override
-    protected Collection<LocalDate> getFixAccrualRanges(NavigableMap<LocalDate, SnapshotRecord> snapshots) {
-        return getFixAccrualRanges(snapshots, Set.of(PastDueOperation.TYPE));
-    }
-
-    @Override
-    public NavigableMap<LocalDate, BigDecimal> getLoanRateRanges(ExampleCredit credit, LocalDate from,
-        LocalDate to) {
-        NavigableMap<LocalDate, BigDecimal> result = new TreeMap<>();
-        result.put(credit.getStartDate(), credit.getCondition().getInterestRate());
-        return result;
+    protected BigDecimal getFeeRate(ExampleCredit credit) {
+        return credit.getCondition().getInterestRate();
     }
 }
--- src/main/java/com/timvero/example/admin/operation/accrual/LateFeeAccrualEngine.java
+++ src/main/java/com/timvero/example/admin/operation/accrual/LateFeeAccrualEngine.java
@@ -5,37 +5,23 @@ import static com.timvero.example.admin.credit.CreditCalculationConfiguration.PA
 import static com.timvero.example.admin.credit.CreditCalculationConfiguration.PAST_DUE_PRINCIPAL;
 
 import com.timvero.example.admin.credit.entity.ExampleCredit;
-import com.timvero.example.admin.operation.pastdue.PastDueOperation;
-import com.timvero.servicing.engine.distribution.SnapshotRecord;
 import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.Collection;
-import java.util.NavigableMap;
 import java.util.Set;
-import java.util.TreeMap;
 
-public class LateFeeAccrualEngine extends BasisAccrualEngine {
+public class LateFeeAccrualEngine extends AbstractAccrualEngine {
 
     @Override
     public String getAccrualAccount() {
         return LATE_FEE;
     }
 
-    @Override
     public Set<String> getBasisAccounts() {
         return Set.of(PAST_DUE_PRINCIPAL, PAST_DUE_INTEREST);
     }
 
     @Override
-    protected Collection<LocalDate> getFixAccrualRanges(NavigableMap<LocalDate, SnapshotRecord> snapshots) {
-        return getFixAccrualRanges(snapshots, Set.of(PastDueOperation.TYPE));
+    protected BigDecimal getFeeRate(ExampleCredit credit) {
+        return credit.getCondition().getLateFeeRate();
     }
 
-    @Override
-    public NavigableMap<LocalDate, BigDecimal> getLoanRateRanges(ExampleCredit credit, LocalDate from,
-        LocalDate to) {
-        NavigableMap<LocalDate, BigDecimal> result = new TreeMap<>();
-        result.put(credit.getStartDate(), credit.getCondition().getLateFeeRate());
-        return result;
-    }
 }
