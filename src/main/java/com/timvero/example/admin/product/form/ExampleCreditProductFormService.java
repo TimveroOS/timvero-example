68c77470258719f001cc219328f46b5ac9208024
Iliya Sinkevich <iliya.sinkevich@timvero.com>
2026-02-10 12:43:03 +0300

    CU-869c2kevr - moved the day counting method to the credit product

--- src/main/java/com/timvero/example/admin/product/form/ExampleCreditProductFormService.java
+++ src/main/java/com/timvero/example/admin/product/form/ExampleCreditProductFormService.java
@@ -7,7 +7,9 @@ import com.timvero.example.admin.product.engine.SimpleScheduledEngine;
 import com.timvero.example.admin.product.entity.ExampleCreditProduct;
 import com.timvero.loan.execution_result.ExecutionResultType;
 import com.timvero.loan.product.entity.CreditType;
+import com.timvero.scheduled.day_count.DayCountMethod;
 import com.timvero.structure.template.form.DocumentTemplateFormService;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,14 +20,17 @@ public class ExampleCreditProductFormService extends EntityFormService<ExampleCr
 
     @Autowired
     private DocumentTemplateFormService documentTemplateService;
+    @Autowired
+    protected Map<String, DayCountMethod> dayCountMethods;
 
     @Override
     protected void assembleEditModel(ExampleCreditProduct entity, CreditProductForm form, Map<String, Object> model) {
-        model.put("productEngines", SimpleScheduledEngine.NAME);
+        model.put("productEngines", List.of(SimpleScheduledEngine.NAME));
         model.put("offerEngineTypes", new ExecutionResultType[]{ExampleDataProcessor.TYPE});
         model.put("creditProductForm", form);
         model.put("creditTypes", CreditType.values(CreditType.class));
         model.put("templates", documentTemplateService.getTemplatesMap(ApplicationContractDocumentCategory.TYPE));
+        model.put("dayCountMethods", dayCountMethods.keySet());
     }
 
 }
